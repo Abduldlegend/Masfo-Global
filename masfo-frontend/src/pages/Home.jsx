ee15eb3e57916c5c07cdcb8a4f1d54927b90cac0
Abdulkadir <musaabdulkadir90@gmail.com>
2026-06-16 13:55:01 +0100

    fix file path in the frontend

--- masfo-frontend/src/pages/Home.jsx
+++ masfo-frontend/src/pages/Home.jsx
@@ -1,10 +1,10 @@
-import { Hero } from '../components/home/Hero'
-import { Features } from '../components/home/Features'
-import { Courses } from '../components/home/Courses'
-import { Stats } from '../components/home/Stats'
-import { Testimonials } from '../components/home/Testimonials'
-import { CTA } from '../components/home/CTA'
-import { Partners } from '../components/home/Partners'
+import { Hero } from '../components/Home/Hero'
+import { Features } from '../components/Home/Features'
+import { Courses } from '../components/Home/Courses'
+import { Stats } from '../components/Home/Stats'
+import { Testimonials } from '../components/Home/Testimonials'
+import { CTA } from '../components/Home/CTA'
+import { Partners } from '../components/Home/Partners'
 
 export const Home = () => {
     return (
